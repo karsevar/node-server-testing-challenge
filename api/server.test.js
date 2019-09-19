9532c4dd0f9dad36df0c719deecc4ed981292105
masonkarsevar <masonkarsevar@gmail.com>
2019-09-19 12:48:58 -0700

    login test written

--- api/server.test.js
+++ api/server.test.js
@@ -18,11 +18,27 @@ describe('server.js root route', () => {
 describe('authRoute.js', () => {
     afterAll(async () => {
         await db('users').truncate();
-    })
+    });
+
     describe('POST /auth/register', () => {
         it('should return id number of newly created user', async () => {
             const response = await request(server).post('/auth/register').send({username: 'mason', password: 'mason'})
             expect(response.body).toEqual(1)
+        })
+        it('should return a 201 ok status code if given unique username', async () => {
+            const response = await request(server).post('/auth/register').send({username: 'alice', password: 'alice'})
+            expect(response.status).toBe(201); 
+        })
+        it('should return a 500 status code if not given a unique username', async () => {
+            const response = await request(server).post('/auth/register').send({username: 'alice', password: 'alice'})
+            expect(response.status).toBe(500); 
+        })
+    })
+
+    describe('POST /auth/login', () => {
+        it('should return status 200 ok if given validate user credentials', async () => {
+            const response = await request(server).post('/auth/login').send({username: 'alice', password: 'alice'})
+            expect(response.status).toBe(200)
         })
     })
 })
