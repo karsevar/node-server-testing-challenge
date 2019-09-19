1f35af4f1fe3d8f437bbd5e4d5d7ba6ea5c35a6b
masonkarsevar <masonkarsevar@gmail.com>
2019-09-19 10:04:24 -0700

    first auth test

--- api/server.test.js
+++ api/server.test.js
@@ -6,6 +6,19 @@ describe('server.js root route', () => {
         const response = await request(server).get('/');
         console.log(response.text)
         expect(response.status).toEqual(201)
-        expect(response.text).toHaveLength(43)
+        expect(response.body).toEqual({message: 'server base route is working!'})
+    })
+    it('should return a response object of {message: "server base route is working!"}', async () => {
+        const response = await request(server).get('/');
+        expect(response.body).toEqual({message: 'server base route is working!'})
+    })
+})
+
+describe('authRoute.js', () => {
+    describe('POST /auth/register', () => {
+        it('should return id number of newly created user', async () => {
+            const response = await request(server).post({username: 'mason', password:'mason'})
+            expect(response.body).toEqual({id: 1})
+        })
     })
 })
